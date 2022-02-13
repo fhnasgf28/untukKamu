df25a6a28199b5375f47388a0fba52e551136f2f
1910631170019_FarhanAssegaf <82705668+fhnasgf28@users.noreply.github.com>
2022-02-13 20:11:39 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".first").addEventListener("click", function () {
+  Swal.fire("Nggak semua orang menjauh karena nggak suka sama kamu, bisa jadi dia menjauh gara-gara kebelet kentut aja, kamu nggak usah baper!");
+});
+
+document.querySelector(".second").addEventListener("click", function () {
+  Swal.fire("untuk kamu", "Selain ada garuda di dadaku, di dadaku juga selalu ada kamu.");
+});
+
+document.querySelector(".third").addEventListener("click", function () {
+  Swal.fire("Besok senin", "Aku nggak sedih kok kalau besok hari senin, aku sedihnya kalau nggak ketemu kamu.", "whoaaaa muntah");
+});
